81c5e056f6bf1756f3e4a0d4caa279af0526e582
Iqram Nugraha <iqram7898@gmail.com>
2020-10-07 10:01:15 +0700

    Membuat tugas 8

---
+++ Tugas-8-Asynchronous/index.js
@@ -0,0 +1,22 @@
+var readBooks = require("./callback.js");
+
+var books = [
+  { name: "LOTR", timeSpent: 3000 },
+  { name: "Fidas", timeSpent: 2000 },
+  { name: "Kalkulus", timeSpent: 4000 },
+];
+
+// Tulis code untuk memanggil function readBooks di sini
+function bacaBooks(waktu, books, indeks = 0) {
+  if (indeks < books.length) {
+    readBooks(waktu, books[indeks], function (sisa) {
+      if (sisa > 0) {
+        bacaBooks(sisa, books, indeks + 1);
+      }
+    });
+  } else {
+    console.log("buku habis");
+  }
+}
+
+bacaBooks(10000, books);
---
+++ Tugas-8-Asynchronous/index2.js
@@ -0,0 +1,25 @@
+var readBooksPromise = require("./promise.js");
+
+var books = [
+  { name: "LOTR", timeSpent: 3000 },
+  { name: "Fidas", timeSpent: 2000 },
+  { name: "Kalkulus", timeSpent: 4000 },
+];
+
+function bacaBooks(waktu, books, indeks = 0) {
+  if (indeks < books.length) {
+    readBooksPromise(waktu, books[indeks])
+      .then((result) => {
+        if (result >= 0) {
+          bacaBooks(result, books, indeks + 1);
+        }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  } else {
+    console.log("buku habis");
+  }
+}
+
+bacaBooks(7000, books);
